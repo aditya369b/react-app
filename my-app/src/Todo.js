c58d6e420a11e54f8ed4b227c624150f2c9f9a59
aditya369b <aditya369b@gmail.com>
2020-02-24 14:05:07 -0800

    Created basic layout for Todo app

---
+++ my-app/src/Todo.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+function Todo(){
+    return (
+        <div>
+            <input type="checkbox" />
+             <p> 1. Text here </p>
+
+             <input type="checkbox" />
+            <p> 2. Text here </p>
+
+            <input type="checkbox" />
+            <p> 3. Text here </p>
+
+            <input type="checkbox" />
+            <p> 4. Text here </p>
+        </div>
+    )
+}
+
+export default Todo
